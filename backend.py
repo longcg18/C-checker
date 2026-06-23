2b1d539927ebd9074fcfe0fa655eb7c1fcd6c57d
Phuc Long Nguyen <90314029+longcg18@users.noreply.github.com>
2026-06-24 03:25:20 +0700

    Fix CORS issue

--- backend.py
+++ backend.py
@@ -810,6 +810,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
+        "https://api.c-checker.io.vn",
+        "https://c-checker.io.vn",
     ],
     allow_credentials=True,
     allow_methods=["*"],
